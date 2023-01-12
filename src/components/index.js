7646a08a22a8fee435aa46ee877f4917a99c2628
popsam22 <sobiechie7@gmail.com>
2023-01-12 01:54:15 -0800

    Initial commit

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import Billing from './Billing'
+import Business from './Business'
+import CardDeal from './CardDeal'
+import Clients from './Clients'
+import CTA from './CTA'
+import Hero from './Hero'
+import Footer from './Footer'
+import Navbar from './Navbar'
+import Stats from './Stats'
+import Testimonials from './Testimonials'
+
+export{
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    CTA,
+    Hero,
+    Footer,
+    Navbar,
+    Stats,
+    Testimonials,
+}
